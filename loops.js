f60ac05a43e908daf2be7ff3b2cc29fbbb0c78a2
Austin Mayden <austin.mayden@gmail.com>
2018-07-10 21:24:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function doWhileLoop(array){
   }
 
   do {
-    console.log(array.length > 0 && maybeTrue())
-  } while (maybeTrue());
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
   return array;
 }
